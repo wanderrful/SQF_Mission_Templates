f60b89f6cc840bb82f89c898a86b15668e97da74
Wanderrful <galois@mentlegen.com>
2017-06-19 07:38:14 +0900

    added shell script
    This shell script is what I use to distribute my mission templates to all relevant folders.

---
+++ refresh-missions.sh
@@ -0,0 +1,46 @@
+#
+#  This script will copy the boilerplate files from my master folder
+#  to each of the appropriate mission folders.  Use this before packaging!
+#
+#  Use this to refresh all mission folders' important files after implementing changes!
+#
+#
+#
+#
+
+
+
+IFS='+' #because of the spaces in the folder pathnames, i need to change this!
+
+
+
+MissionFolder='/cygdrive/d/docs/arma 3 - Other Profiles/sixtyfour/missions/_fuckfuckgames'
+MasterFolder='/cygdrive/d/docs/arma\ 3\ -\ Other\ Profiles/sixtyfour/_library/templates'
+
+
+
+for template in $MasterFolder/*
+do
+	if [ -d $template ]
+	then
+	    for folder in $MissionFolder/*
+	    do
+		TemplateName=${template##/*/}
+		
+		prefix=${folder##/*/}
+		
+		MissionType={$prefix%%_*}
+		
+		
+		if [ $TemplateName == ${prefix%%_*} ]
+		then
+		    echo 'Distributing the master files for the ' $TemplateName ' template...'
+	    	    cp -r $template/* $folder/
+		fi
+	    done
+	fi
+done
+
+
+
+echo 'Done!'
